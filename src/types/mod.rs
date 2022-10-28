875f87e06bdec5a83e0208029df040ec35978ac2
butabara <butabara@laminar.markets>
2022-10-28 23:17:08 +0800

    serialize u64 to string

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -4,12 +4,12 @@ pub mod events;
 pub mod order;
 
 use serde::de::{Error, Visitor};
-use serde::{Deserialize, Deserializer};
+use serde::{Deserialize, Deserializer, Serializer};
 use std::fmt::Formatter;
 use std::marker::PhantomData;
 use std::str::FromStr;
 
-pub(crate) fn deserialize_from_str<'de, D, T>(deserializer: D) -> Result<T, D::Error>
+pub fn deserialize_from_str<'de, D, T>(deserializer: D) -> Result<T, D::Error>
 where
     T: Deserialize<'de> + FromStr,
     D: Deserializer<'de>,
@@ -40,4 +40,21 @@ where
     deserializer.deserialize_any(StringVisitor::<T> {
         phantom: PhantomData::default(),
     })
+}
+
+pub fn u64_to_str<S>(value: &u64, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    serializer.serialize_str(&value.to_string())
+}
+
+pub fn u64_option_to_str<S>(value: &Option<u64>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    match value {
+        None => serializer.serialize_none(),
+        Some(v) => serializer.serialize_str(&v.to_string()),
+    }
 }
